a0460bc1089397eef1b8520f9e67220c3b0dd780
Nehabisht773 <nehbisht@deloitte.com>
2025-03-12 17:50:10 +0530

    server cross side scripting and JSX

--- App.js
+++ App.js
@@ -11,18 +11,25 @@ console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 //root.render converts the heading createElement oject to HTML object and replaces it in DOM. (Not append )
- 
+
+const reactElement  = <span>Yayyy!!!</span>
 //Create react element using JSX
 const headingJSX = (
   <h1 id="heading" className="heading" tabIndex={5}>
-    Namaste React using JSX
+     Namaste React using JSX
   </h1>
 ); //This is not HTML using JS. JSX is HTML/XML like syntax.
 
+const data = api.getData(); 
 //react component
 const HeadingComponent = () => {
     return (
+        <>
+        {/* {data} cross side scripting JSX sanitises this data */}
+         {headingJSX} 
+         <h2>{1000+1000}</h2>
         <h1>Namaste React Heading component</h1>
+        </>
     )
 }
 
